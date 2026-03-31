f88bbdd402a7950fec87666cb51471fec1eebc95
Andres2803 <94024580+ADCcodes@users.noreply.github.com>
2026-03-31 13:49:02 -0500

    Configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,22 @@
-import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
+import vercel from '@astrojs/vercel/static'; // <--- IMPORTA ESTO
 import sanity from '@sanity/astro';
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'astro/config';
 
 export default defineConfig({
+  output: 'hybrid', // <--- CAMBIA ESTO (Permite el Studio de Sanity)
+  adapter: vercel(), // <--- AGREGA ESTO
   vite: {
     plugins: [tailwindcss()]
   },
   integrations: [
     react(), 
     sanity({
-      projectId: '7451e60s', // <--- Solo pega el ID que sacaste de la web
+      projectId: '7451e60s',
       dataset: 'production',
       useCdn: true,
-      studioAt: '/admin', // <--- Esto es clave: El CMS vivirá en tuweb.com/admin
+      studioAt: '/admin', 
     })
   ]
 });
