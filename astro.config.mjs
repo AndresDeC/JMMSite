7a8f30be80a3c100e8fe752c1cb9deae7e119cba
Andres2803 <94024580+ADCcodes@users.noreply.github.com>
2026-03-31 14:28:13 -0500

    Configuracion de versiones

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,30 @@
 import react from '@astrojs/react';
-import vercel from '@astrojs/vercel'; // <--- IMPORTA ESTO
+import vercel from '@astrojs/vercel'; // Asegúrate que el import sea así
 import sanity from '@sanity/astro';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  adapter: vercel(),
+  // Forzamos el modo servidor para que el admin funcione
+  output: 'server', 
+  adapter: vercel({
+    webAnalytics: { enabled: true },
+    edgeMiddleware: true // <--- Cambiamos a arquitectura Edge
+  }),
+  integrations: [
+    react(),
+    sanity({
+      projectId: '7451e60s',
+      dataset: 'production',
+      useCdn: true,
+    }),
+  ],
   vite: {
     plugins: [tailwindcss()],
     build: {
-      chunkSizeWarningLimit: 2000, // Sube el límite de aviso a 2MB
+      chunkSizeWarningLimit: 2000,
       rollupOptions: {
         output: {
-          // Esto ayuda a que Sanity no sea un solo bloque gigante
           manualChunks(id) {
             if (id.includes('node_modules/sanity')) {
               return 'sanity';
@@ -21,13 +33,5 @@ export default defineConfig({
         }
       }
     }
-  },
-  integrations: [
-    react(), 
-    sanity({
-      projectId: '7451e60s',
-      dataset: 'production',
-      useCdn: true,
-    })
-  ]
+  }
 });
