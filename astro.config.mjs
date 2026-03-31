374f40b93e0d890757029b0905d40358b7cf0ffd
Andres2803 <94024580+ADCcodes@users.noreply.github.com>
2026-03-31 12:48:01 -0500

    Config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,20 @@
 // @ts-check
+import react from '@astrojs/react';
+import sanity from '@sanity/astro';
+import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
-import tailwindcss from '@tailwindcss/vite';
-
-import react from '@astrojs/react';
-
-import sanity from '@sanity/astro';
-
-// https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
-
-  integrations: [react(), sanity()]
+  integrations: [
+    react(), 
+    sanity({
+      // Usamos import.meta.env para que Astro lo jale en el build
+      projectId: '65f6y8u9', 
+      dataset: 'production',
+      useCdn: true,
+    })
+  ]
 });
