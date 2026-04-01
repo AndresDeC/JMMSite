54c49fab58cf508dfc05f70657bda20f53c0fec7
Andres2803 <94024580+ADCcodes@users.noreply.github.com>
2026-04-01 10:50:21 -0500

    Sanity CMS

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,16 +1,17 @@
-// sanity.config.ts
-import { defineConfig } from 'sanity';
-import { structureTool } from 'sanity/structure';
+import { defineConfig } from 'sanity'
+import { structureTool } from 'sanity/structure'
+import { schemaTypes } from './schemaTypes'; // <--- Asegúrate de que apunte a la carpeta
 
 export default defineConfig({
   name: 'default',
   title: 'JM Monterrey Admin',
+
   projectId: '7451e60s',
   dataset: 'production',
+
   plugins: [structureTool()],
+
   schema: {
-    types: [
-      schemaTypes,
-    ],
+    types: schemaTypes, // <--- Esto es lo que le dice al panel qué mostrar
   },
-});
+})
